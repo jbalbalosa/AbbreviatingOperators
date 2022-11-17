07df498e3b2dad06a612e361d11b838cf218bd66
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-11-17 14:30:17 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,45 @@ public class Main {
         System.out.println("Udemy: Java Programming Masterclass");
         System.out.println("Abbreviating Operators");
 
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println(previousResult);
+        result = result -1;
+        System.out.println(result);
+
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3; //the remainder of (4 % 3) = 1
+        System.out.println(result);
+
+
+        //Abbreviating Operators
+        result++;
+        System.out.println("1 + 1 = " + result);
+
+        result--;
+        System.out.println("2 -1 = " + result);
+
+        result += 2;
+        System.out.println("1 + 2 = " + result);
+
+        //result = result * 10;
+        result *= 10;
+        System.out.println("3 * 10 = "  + result);
+
+        //result = result / 5;
+        result /= 5;
+        System.out.println("30 / 5 = " + result);
+
+        //result = result - 2
+        result -= 2;
+        System.out.println("6 - 2 = " + result);
+
 
     }
 }
